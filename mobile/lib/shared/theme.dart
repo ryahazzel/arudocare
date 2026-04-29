266916d1fef622df18fb91a9f7a39e6884055ebc
RZ <105061149+ryahazzel@users.noreply.github.com>
2026-04-29 10:13:42 +0700

    flutter setup

---
+++ mobile/lib/shared/theme.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+const Color kPrimaryColor = Color(0xFF39A28F);
+const Color kBackgroundColor = Color(0xFFF9F9F9);
+const Color kWhiteColor = Color(0xFFFFFFFF);
+const Color kOrangeColor = Color(0xFFF2994A);
